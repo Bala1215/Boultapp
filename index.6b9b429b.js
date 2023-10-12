705c8f24822b8aa2d3a48f1c9731152dc2db6790
Code Sandbox <hello@codesandbox.io>
2023-10-12 07:48:23 +0000

    Deployment from Codesandbox.

---
+++ index.6b9b429b.js
@@ -0,0 +1,3 @@
+const e=[...document.querySelectorAll(".switcher")];e.forEach(t=>{t.addEventListener("click",function(){e.forEach(e=>e.parentElement.classList.remove("is-active")),this.parentElement.classList.add("is-active")})});//# sourceMappingURL=index.6b9b429b.js.map
+
+//# sourceMappingURL=index.6b9b429b.js.map
